9645ea2378ddc53fa17ad1f77a38dee0cabacd8c
jp <jpjp8414@gmail.com>
2020-04-09 11:51:18 +0900

    Feat: Add Ingredient Cihp View in Calendar Detail

--- app/src/main/java/com/jp/babyfood/ui/calendardetail/CalendarDetailFragment.kt
+++ app/src/main/java/com/jp/babyfood/ui/calendardetail/CalendarDetailFragment.kt
@@ -1,8 +1,14 @@
 package com.jp.babyfood.ui.calendardetail
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.text.style.ImageSpan
 import android.view.View
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.chip.ChipDrawable
 import com.jp.babyfood.R
 import com.jp.babyfood.databinding.FragmentCalendarDetailBinding
 import com.jp.babyfood.ui.base.BaseFragment
@@ -22,6 +28,50 @@ class CalendarDetailFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        var SpannedLength = 0
+
+        viewBinding.ingredientEdit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                val str = editable.toString()
+                LOGI("BF_TAG", "afterText : $str, SpannedLength : $SpannedLength")
+
+                if (!TextUtils.isEmpty(str) && str.subSequence(
+                        str.length - 1,
+                        str.length
+                    ) == "\n"
+                ) {
+                    editable.delete(editable.length - 1, editable.length)
+
+                    val chip = ChipDrawable.createFromResource(requireContext(), R.xml.chip)
+                    chip.text = editable.subSequence(SpannedLength, editable.length)
+                    chip.setBounds(0, 0, chip.intrinsicWidth, chip.intrinsicHeight)
+
+                    val span = ImageSpan(chip)
+                    editable.setSpan(
+                        span,
+                        SpannedLength,
+                        editable.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+
+                    SpannedLength = editable.length
+                }
+                if (TextUtils.isEmpty(str)) {
+                    SpannedLength = 0
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(charSequence: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//                if (charSequence?.length == SpannedLength - chipLength) {
+//                    SpannedLength = charSequence?.length
+//                }
+            }
+
+        })
         LOGI("BF_TAG", "Day : ${args.day}")
     }
 }
