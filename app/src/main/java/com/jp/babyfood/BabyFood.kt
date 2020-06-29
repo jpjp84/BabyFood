bed12dd8ef15e5d47de036174d585ec6ad67a1d3
jp <jpjp8414@gmail.com>
2020-06-29 15:46:47 +0900

    Feat: Logger 추가 및 설정

--- app/src/main/java/com/jp/babyfood/BabyFood.kt
+++ app/src/main/java/com/jp/babyfood/BabyFood.kt
@@ -2,8 +2,11 @@ package com.jp.babyfood
 
 import androidx.multidex.MultiDex
 import com.jp.babyfood.di.component.DaggerAppComponent
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+
 
 open class BabyFood : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
@@ -13,5 +16,6 @@ open class BabyFood : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         MultiDex.install(this)
+        Logger.addLogAdapter(AndroidLogAdapter())
     }
 }
